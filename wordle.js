7b8f5c84dd3e5ea4e8d978f4543aa8efcda4b40f
JustinPong <Justinpong0416@gmail.com>
2022-03-16 02:59:47 +0800

    able to type and backspace

--- wordle.js
+++ wordle.js
@@ -8,18 +8,49 @@ let col = 0;
 let gameOver = false;
 let word = "SQUID";
 
-window.onload = function() {
+window.onload = function () {
     intialize();
 }
 
 function intialize() {
-    for(let r = 0; r < height; r++) {
-        for(let c = 0; c < width; c++) {
+    for (let r = 0; r < height; r++) {
+        for (let c = 0; c < width; c++) {
             let tile = document.createElement("span");
             tile.id = r.toString() + "-" + c.toString();
             tile.classList.add("tile");
-            tile.innerText = "Test";
+            tile.innerText = "";
             document.getElementById("board").appendChild(tile);
         }
     }
+
+    document.addEventListener("keyup", (e) => {
+        if (gameOver) {
+            return;
+        }
+        // alert(e.code);
+        if ("KeyA" <= e.code && e.code <= "KeyZ") {
+            if (col < width) {
+                let currTile = document.getElementById(row.toString() + '-' + col.toString());
+                if (currTile.innerText === "") {
+                    currTile.innerText = e.code[3];
+                    col += 1;
+                }
+            }
+        } else if (e.code === "Backspace") {
+            if (0 < col && col <= width) {
+                col -= 1;
+            }
+            let currTile = document.getElementById(row.toString() + "-" + col.toString());
+            currTile.innerText = "";
+        } else if (e.code === "Enter") {
+            update();
+            row += 1;
+            col = 0;
+        }
+
+        if(!gameOver && row == height) {
+            gameOver = true;
+            document.getElementById("answer").innerText = word;
+        }
+    })
 }
